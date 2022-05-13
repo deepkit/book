2ef0a6d3816a581e6e564de558e16f139f9cc6ad
Marc J. Schmidt <marc@marcjschmidt.de>
2022-05-14 00:15:05 +0200

    add serialization and cross-links

--- build-pdf.sh
+++ build-pdf.sh
@@ -1,4 +1,4 @@
 
 mkdir -p build;
-asciidoctor -b docbook src/index.adoc -o build/index.xml
+asciidoctor -b docbook5 src/index.adoc -o build/index.xml
 pandoc --from docbook --toc --to latex --output build/deepkit-book.pdf build/index.xml
