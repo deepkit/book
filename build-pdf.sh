c2a65fba87761b31d3e6898dae2ac41a8564712c
Marc J. Schmidt <marc@marcjschmidt.de>
2022-05-15 00:27:22 +0200

    validation [custom validator] + http [events, scope, resolver]

--- build-pdf.sh
+++ build-pdf.sh
@@ -1,4 +1,8 @@
 
 mkdir -p build;
+
+echo Build Docbook
 asciidoctor -b docbook5 src/index.adoc -o build/index.xml
-pandoc --from docbook --toc --to latex --output build/deepkit-book.pdf build/index.xml
+
+echo Build PDF
+pandoc --from docbook --toc --to latex --resource-path src/ --output build/deepkit-book.pdf build/index.xml
